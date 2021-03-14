36a70e2936d234bb7f186db3a7577ca952fe9553
Yanick <tyankee15@gmail.com>
2021-03-13 21:44:38 -0300

    Add DOM elements to get values from the form

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', search => {
+    const form = document.getElementsByTagName('form')[0];
+    const input = document.getElementById('search');
+    const location = input.value;
+    form.reset();
+});
